75442be13b4cb96ecbffe90269d25a8ed62dc92a
Miguel Esteban Gutiérrez <m.esteban.gutierrez@gmail.com>
2016-03-14 23:24:37 +0100

    Improved QA script of the Travis CI build configuration, continued
    - Fixed failure when checking the result from the server connectivity
      checking.
    - Enabled using which application to use for carrying out the checks
      (curl/wget) leveraging the CHECKER environment variable. By default,
      wget will be used.
    - Enabled showing additional log messages w.r.t. the checking process
      leveraging the DEBUG environment variable when its value is 'verbose'.

--- .travis/qa.sh
+++ .travis/qa.sh
@@ -1,30 +1,84 @@
 #!/bin/bash
 
-function checkSonarQubeServer() {
-  error=1
-  echo "Checking SonarQube Server..."
+function logCheckResults() {
+  if [ "${DEBUG}" = "verbose" ];
+  then
+    echo "Trace:"
+    cat log.txt
+    echo "Response:"
+    cat data.txt
+  fi
+}
+
+function checkWithCurl() {
+  if [ "${DEBUG}" = "verbose" ];
+  then
+    curl --version
+  fi
   curl --head --silent --verbose --connect-timeout 5 "$1" > data.txt 2> log.txt
-  if [ "$?" = "0" ];
+  error=$?
+  logCheckResults
+  return "$error"
+}
+
+function checkWithWget() {
+  if [ "${DEBUG}" = "verbose" ];
+  then
+    wget --version
+  fi
+  # Write response (and headers) to 'data.txt', (verbose) activity to 'log.txt'
+  # including additional debug entries (e.g., requests and responses), with a
+  # generic timeout of 5s, and retry only once.
+  # In addition, discard the additional output (both standard and error)
+  wget --output-document=data.txt --output-file=log.txt --verbose --debug --timeout=5 --tries 1 --save-headers "$1" > error.log 2>&1
+  error=$?
+  if [ "$error" != "0" ];
+  then
+    # If a failure happens, but the server sent a response...
+    if [ "$(grep -c "response begin" log.txt)" -gt 0 ]
+    then
+      # ... we extract the response from the log...
+      from=$(awk '/response begin/{ print NR; exit }' log.txt)
+      to=$(awk '/response end/{ print NR; exit }' log.txt)
+      head -n $((to - 1)) log.txt | tail -n $((to - from - 1)) > data.txt
+      # ... and clear the failure status.
+      error=0
+    fi
+  fi
+  logCheckResults
+  return "$error"
+}
+
+function checkSonarQubeServer() {
+  echo "Checking SonarQube Server..."
+
+  case "${CHECKER}" in
+    curl ) checkWithCurl "$1";;
+    *    ) checkWithWget "$1";;
+  esac
+
+  error=$?
+  if [ "$error" = "0" ];
   then
     status=$(head -n 1 data.txt | awk '{print $2}')
-    if [ "$status" = "200" ];
+    if [ "$status" != "200" ];
     then
-      error=0
-    else
       echo "Skipped SonarQube analysis (${TRAVIS_BRANCH}). SonarQube Server is not available ($status)"
+      error=1
     fi
   else
     echo "Skipped SonarQube analysis (${TRAVIS_BRANCH}). Could not connect to SonarQube Server: "
     cat log.txt
+    error=2
   fi
   rm data.txt
   rm log.txt
-  return $error
+  return "$error"
 }
 
 function analyzeBranch() {
   checkSonarQubeServer "$1"
-  if [ "$1" = "0" ];
+  if [ "$?" = "0" ];
   then
     if [ "$2" != "porcelain" ];
     then
